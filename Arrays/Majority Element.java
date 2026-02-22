2bcb224e4718cc5aef87ffc2ee8460507d1f4b97
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-02-22 21:49:06 +0530

    majorityElement

--- Arrays/Majority Element.java
+++ Arrays/Majority Element.java
@@ -1,11 +1,9 @@
-//solved on interviewBit.com
-public class Solution {
-    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
-    public int majorityElement(final int[] A) {
-        int n = A.length;
+class Solution {
+    public int majorityElement(final int[] nums) {
+        int n = nums.length;
         Map<Integer, Integer> freq = new HashMap<>();
         for(int i=0;i<n;i++){
-            freq.put(A[i],freq.getOrDefault(A[i],0)+1);
+            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
         }
         for (Integer key : freq.keySet()){
             if(freq.get(key)>n/2){

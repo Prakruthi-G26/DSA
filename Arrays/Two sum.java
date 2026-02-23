8d5b5d6320dfa38fa1bc2a1035e3a45863fb5754
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-02-23 22:43:46 +0530

    Add HashMap solution for two-sum problem
    Implemented a HashMap approach for the two-sum problem to improve efficiency.

--- Arrays/Two sum.java
+++ Arrays/Two sum.java
@@ -17,7 +17,19 @@ class Solution {
 //space complexity - O(1)
 
 //HashMap approach
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map <Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int num = target - nums[i];
+            if(map.containsKey(num) && map.get(num)!=i){
+                return new int [] {i, map.get(num)};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
 
 //time complexity - O(n^2)
 //space complexity - O(1)

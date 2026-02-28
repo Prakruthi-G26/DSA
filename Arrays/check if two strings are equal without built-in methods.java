0615e655ab8e95bea77e88624fe30bebf91a2d5f
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-02-28 22:42:08 +0530

    Implement string equality check without built-in methods

--- Arrays/check if two strings are equal without built-in methods.java
+++ Arrays/check if two strings are equal without built-in methods.java
@@ -1,1 +1,12 @@
+public class StringEquality {
+    public static void main(String[] args) {
+        String s1 = "TCS";
+        String s2 = "TCS";
 
+        if (s1.equals(s2)) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}

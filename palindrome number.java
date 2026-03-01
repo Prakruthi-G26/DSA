c003a26ec76a48c6187c8463437ca1069317bc46
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-03-01 17:50:47 +0000

    adding solution to palindrome number problem

---
+++ palindrome number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reversed = 0;
+        int original = x;
+        while(x>0){
+            reversed = reversed*10 + x%10;
+            x = x/10;
+        }
+        return original == reversed;
+    }
+}

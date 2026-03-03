23ce0bc67f0df5268ae4558e167fbea443703ac8
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-03-03 22:41:54 +0530

    Add optimized mergeAlternately method using StringBuilder
    Implemented an optimized approach using StringBuilder to merge two strings alternately.

--- Strings/Merge strings alternately.java
+++ Strings/Merge strings alternately.java
@@ -31,3 +31,31 @@
 
 //time complexity - O((n+m)^2)
 //space complexity - O(n+m)
+
+
+//using stringBuilder class - optimised approach
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i=0, j=0;
+        StringBuilder word = new StringBuilder();
+
+        while(i<word1.length() && j<word2.length()){
+            word.append(word1.charAt(i));
+            word.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+
+        while(i<word1.length()){
+            word.append(word1.charAt(i));
+            i++;
+        }
+
+        while(j<word2.length()){
+            word.append(word2.charAt(j));
+            j++;
+        }
+
+        return word.toString();
+    }
+}

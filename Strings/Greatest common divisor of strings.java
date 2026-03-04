a33917e775a6e398cfdf20950ae130728d7ea675
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-03-04 23:40:29 +0530

    Add solution to find GCD of two strings
    Implement GCD of two strings based on substring criteria.

--- Strings/Greatest common divisor of strings.java
+++ Strings/Greatest common divisor of strings.java
@@ -1,1 +1,22 @@
+//input - two strings str1, str2
+//output - string t that devides both str1 and str2
+//t must be a substring of both str1 and str2 to devide both
+//and both string must not contains anything other than characters of t
+//similar to largest substring 
+// GCD 
 
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        String t = "";
+        if((str1+str2).equals(str2+str1)){
+            int n = gcd(str1.length(),str2.length());
+            t = t+str1.substring(0,n);
+        }
+        return t;
+    }
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}

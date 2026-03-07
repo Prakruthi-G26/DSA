3d147a16b45867264a8ec68a410923fa80d0a700
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-03-07 23:14:08 +0530

    Update can place flowers.java

--- Arrays/can place flowers.java
+++ Arrays/can place flowers.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for(int i = 0; i < flowerbed.length; i++){
+            if(flowerbed[i] == 0){
+                if(i == 0 || flowerbed[i-1] == 0){
+                    if(i == flowerbed.length-1 || flowerbed[i+1]==0){
+                        flowerbed[i] = 1;
+                        n = n - 1;
+                    }
+                }
+            }
+        }
+        if(n <= 0){
+            return true;
+        }
+        return false;
+    }
+}

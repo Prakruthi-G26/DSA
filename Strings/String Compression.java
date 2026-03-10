d244e3b63fce0a123bb3ddbad44a9f469e3311f7
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-03-10 16:00:57 +0530

    Update String Compression.java

--- Strings/String Compression.java
+++ Strings/String Compression.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public int compress(char[] chars) {
+        int write = 0;
+        int i = 0;
+        int count = 0;
+        while(i < chars.length){
+            j = i;
+            while(j < n && chars[j] == chars[i]){
+                j++;
+            }
+            count = j - ;
+        }
+        chars[write++] = chars[i]
+        if count > 1{
+            for c in String.valueOf(count).toCharArray(){
+                chars[write++] = c;
+            }
+        }
+        i = j;
+    }
+    return chars.length;
+}

c63a4e22b663dc25acc5fa8261ba21570162e458
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-03-12 23:45:59 +0530

    Update Move Zeroes.java

--- Arrays/Move Zeroes.java
+++ Arrays/Move Zeroes.java
@@ -16,3 +16,19 @@ class Solution {
 
 // Time Complexity: O(n)
 // Space Complexity: O(1)
+
+!---------------------------------------------------------SWapping--------------------------------------------------------------------
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}

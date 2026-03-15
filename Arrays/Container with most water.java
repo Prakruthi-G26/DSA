7fe36ef5b2e8f91d9989d4f0a8341fb5fc834775
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-03-15 22:57:47 +0530

    Update Container with most water.java

--- Arrays/Container with most water.java
+++ Arrays/Container with most water.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int maxArea(int[] height) {
+        int water = 0;
+        int i = 0;
+        int j = height.length - 1;
+        while(i < j){
+            if(height[i] < height[j]){
+                water = Math.max(water, (j-i)*height[i]);
+                i++;
+            }
+            else{
+                water = Math.max(water, (j-i)*height[j]);
+                j--;
+            }
+        }
+        return water;
+    }
+}

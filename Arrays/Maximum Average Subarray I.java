be287dd4ac57d52d82a35aeabde12dd24e01f027
Prakruthi-G26 <prakruthig9437@gmail.com>
2026-03-25 23:19:40 +0530

    Update Maximum Average Subarray I.java

--- Arrays/Maximum Average Subarray I.java
+++ Arrays/Maximum Average Subarray I.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double maxSum = Double.MIN_VALUE;
+        double sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += nums[i];
+        }
+        maxSum = sum;
+        for(int i = k; i < nums.length; i++){
+            sum = sum + nums[i];
+            sum = sum - nums[i-k];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum / k;
+    }
+}
+//
